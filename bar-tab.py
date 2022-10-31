1cbda6231f662b0ffadd7080dffba1365ce845a5
LH <lindsaynhenderson@gmail.com>
2022-10-31 15:14:24 -0500

    first commit

---
+++ bar-tab.py
@@ -0,0 +1,22 @@
+#calc area of a cirlcle
+
+##radius = input('enter radius')
+#input is a string.  need int wrapped to convert string to number
+#area = 3.142*int(radius)**2
+#print('the area of your circle is', area)
+
+#age = int(input('enter your age: '))
+
+#if age < 10:
+  # code block
+ # print('you are too young for margaritas')
+
+age = 25
+num = 0
+
+while num < age:
+  if num % 2 == 0:
+    print(num)
+  if num == 5:
+    print(num,'golden riinggsss!')
+  num +=1
